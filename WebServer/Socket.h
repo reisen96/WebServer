b0020a52ca38f6c5ba956caa24ed27904d7b37fc
Zvi Naor Cohen <zvinaorco@mta.ac.il>
2023-05-17 19:41:16 +0300

    Added functions for Client, Socket, Webserver.

--- WebServer/Socket.h
+++ WebServer/Socket.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "NetworkException.h"
+#include "Client.h"
 
 constexpr int bufferSize = 255;
 
@@ -18,8 +19,10 @@ private:
 	const int majorWinsockVersion = 2;
 	const int minorWinsockVersion = 2;
 	SOCKET windowsSocket;
+	Client client;
 	sockaddr_in socketAddress;
 	SocketState socketState;
+
 	char socketBuffer[bufferSize];
 
 	Socket(SOCKET& windowsSocket, sockaddr_in& socketAddress, SocketState socketState = SocketState::Inactive);
@@ -41,13 +44,15 @@ public:
 	bool listenState() { return socketState == SocketState::Listen; }
 	bool receiveState() { return socketState == SocketState::Receive; }
 	bool sendState() { return socketState == SocketState::Send; }
+	Client getClient() { return client; }
 
 	void initialize(const std::string& ipAddress, unsigned short port, int type = SOCK_DGRAM, int protocol = IPPROTO_UDP);
+
 	void bindToPort();
 	void setListen(int backlog = 0);
 	void setMode(bool blocking);
 	void setReceive() { socketState = SocketState::Receive; }
 	void setSend() { socketState = SocketState::Send; }
+	void setInactive() { socketState = SocketState::Inactive; }
 	void close();
 };
-
